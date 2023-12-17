91af5149627e85e0ae7d29468b1eb859ba5e729a
Zankimen <152904812+Zankimen@users.noreply.github.com>
2023-12-18 06:44:41 +0700

    Delete src directory

--- src/todo/list/override.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package todo.list;
-
-public @interface override {
-
-}
